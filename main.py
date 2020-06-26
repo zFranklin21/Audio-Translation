d634049a161d46730b693304df276bd7e32a8d82
zFranklin21 <fzheng2021@gmail.com>
2020-06-25 17:01:16 -0700

    Speech synthesis added, translation next

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from time import sleep
 try:
     import pyaudio
     import speech_recognition as sr
+    import pyttsx3
     from googletrans import Translator
 except ImportError:
     print("Error: PyAudio and SpeechRecognition packages are needed.\
-    \nPlease run $ pip install pyaudio speechrecognition googletrans");
+    \nPlease run $ pip install pyaudio speechrecognition googletrans pyttsx3");
     sys.exit();
 
 def userInput(recordChoice):
@@ -41,27 +42,42 @@ def readFile(rawFile, r):
     return audio
 
 
-def processAudio(audio, r):
+def processAudio(audio, r, ss, lang):
     try:
         print("\nProcessing audio...")
-        processed = r.recognize_google(audio)
+        processed = r.recognize_google(audio, language = lang)
         # r.recognize_google(audio, language="fr-FR")
         # https://cloud.google.com/speech-to-text/docs/languages
         print("Audio processed.")
         print("\nTranscription of audio:\n")
         print(processed)
+        ss.say(processed)
+        ss.runAndWait()
     except sr.UnknownValueError:
         print("Sorry, audio was unable to be processed. Check for excessive background noise.")
 
 def main():
     # Instantiation of modules
+    language = "en_US"
+    voiceID = None
     r = sr.Recognizer()
     tl = Translator()
+    ss = pyttsx3.init()
+
+    voices = ss.getProperty('voices')
+    for voice in voices:
+        print(voice)
+        if ''.join(voice.languages).lower() == language.lower():
+            ss.setProperty('voice', voice.id)
+            break
+        elif language.lower() in voice.id.lower():
+            voiceID = voice.id
+            break
 
     recordChoice = input("\nDo you have a file you would like translated? ").lower()
     rawFile = userInput(recordChoice)
     audio = readFile(rawFile, r)
-    processAudio(audio, r)
+    processAudio(audio, r, ss, language)
 
     print("\nTerminating program.")
     sys.exit()
